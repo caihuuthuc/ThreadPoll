2b5dfad2f326c453531bf218139b79f81de55ccd
Thuc Cai Huu <huuthuc997@gmail.com>
2023-11-27 21:59:28 +0700

    Update accumulation.cpp

--- examples/accumulation.cpp
+++ examples/accumulation.cpp
@@ -1,4 +1,4 @@
-#include "../thread_pool.hpp"
+#include "../ThreadPool.hpp"
 #include <list>
 #include <iostream>
 #include <numeric>
@@ -12,7 +12,7 @@ struct accumulation_block {
 
 template <typename T, typename Iterator = typename std::list<T>::iterator>
 T accumulation(Iterator first, Iterator last, T init) {
-  thread_pool pool(2);
+  ThreadPool pool(2);
   unsigned long const length = std::distance(first, last);
   if (!length) return init;
   unsigned long const block_size = 25;
